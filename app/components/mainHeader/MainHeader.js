dcd2e06e6a8689bbe173de77d3e12a27f4c4ef20
uniboxx <unibox@duck.com>
2024-09-13 04:39:13 +0200

    lesson 430

--- app/components/mainHeader/MainHeader.js
+++ app/components/mainHeader/MainHeader.js
@@ -4,6 +4,8 @@ import classes from './MainHeader.module.css';
 import Image from 'next/image';
 import MainHeaderBackground from './MainHeaderBackground';
 
+import NavLink from './NavLink';
+
 function MainHeader() {
   return (
     <>
@@ -17,10 +19,10 @@ function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
---
+++ app/components/meals/MealsGrid.js
@@ -0,0 +1,16 @@
+import MealItem from './MealItem';
+import classes from './MealsGrid.module.css';
+
+function MealsGrid({ meals }) {
+  return (
+    <ul className={classes.meals}>
+      {meals.map(meal => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default MealsGrid;
