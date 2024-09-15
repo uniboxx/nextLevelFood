08be0f388bccac69ce88ead93bcc672303f8ba75
uniboxx <unibox@duck.com>
2024-09-15 11:33:50 +0200

    lesson 456 and finish project

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,19 @@ import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 function MealDetailsPage({ params }) {
   const slug = params.mealSlug;
   const meal = getMeal(slug);
--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,11 @@ import { getMeals } from '@/lib/meals';
 import { Suspense } from 'react';
 import MealsLoadingPage from './MealsLoadingPage';
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
 async function Meals() {
   const meals = await getMeals();
 
