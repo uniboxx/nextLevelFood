37241cb6c154d25110d42989a670032c9a883f30
uniboxx <unibox@duck.com>
2024-10-16 13:31:14 +0200

    add headers

--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'ntytxecxpzrxqxwbalcb.supabase.co',
-        pathname: '/storage/v1/object/public/meals-images/**',
+        pathname: '/storage/v1/object/public/meals-images/*',
       },
       {
         protocol: 'https',
