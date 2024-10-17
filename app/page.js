631dffb435ddeffe6f9b30baf10ae35eb20cdc1c
uniboxx <unibox@duck.com>
2024-10-17 16:10:50 +0200

    add responsive

--- app/page.js
+++ app/page.js
@@ -9,15 +9,15 @@ export default function Home() {
         <div className={classes.slideshow}>
           <ImageSlideshow />
         </div>
-        <div>
-          <div className={classes.hero}>
-            <h1>NextLevel Food for NextLevel Foodies</h1>
-            <p>Taste & share food from all over the world.</p>
-          </div>
-          <div className={classes.cta}>
-            <Link href="/community">Join the Community</Link>
-            <Link href="/meals">Explore Meals</Link>
-          </div>
+
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p>Taste & share food from all over the world.</p>
+        </div>
+
+        <div className={classes.cta}>
+          <Link href="/community">Join the Community</Link>
+          <Link href="/meals">Explore Meals</Link>
         </div>
       </header>
       <main>
